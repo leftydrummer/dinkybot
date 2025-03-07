c3419a2422d2adebb38e9b2247061e369b208b92
Neill Wolf <neillawolf@gmail.com>
2025-03-07 00:17:32 -0600

    setup for test env

--- dinkybot.py
+++ dinkybot.py
@@ -14,11 +14,10 @@ intents = discord.Intents.default()
 intents.message_content = True
 dinkybot = commands.Bot(command_prefix="!", intents=intents)
 
+
 """
 EVENT HANDLERS @
 """
-
-
 # Runs after the bot is authenticated and synced with Discord
 # For all guilds, find the general channel and post the 'ready' message
 @dinkybot.event
@@ -31,16 +30,21 @@ async def on_ready():
         print(e)
 
     for guild in dinkybot.guilds:
-        for channel in guild.channels:
-            if channel.name == "general" and isinstance(channel, discord.TextChannel):
-                await channel.send("Hello World! I'm awake!")
-                break
+        print(
+            f"{dinkybot.user} is connected to the following guild: {guild.name} (id: {guild.id})"
+        )
+        for channel in guild.text_channels:
+            print(f"Found text channel: {channel.name} (id: {channel.id})")
 
 
-# If a message in the server contains the string "this is really weird" or "this is weird", post the 'weird' GIF
+# If a message in the server contains the string "this is really weird" or "this is weird", post the 'weird' GIF and reply to the user
 @dinkybot.event
 async def on_message(message):
-    if "this is really weird" in str.lower(message.content) or "this is weird" in str.lower(message.content):
+
+    if "this is really weird" in str.lower(
+        message.content
+    ) or "this is weird" in str.lower(message.content):
+        await message.channel.send(f"It sure is, {message.author.mention}")
         await utils.post_gif("weird-optimized", message)
     await dinkybot.process_commands(message)
 
@@ -48,7 +52,6 @@ async def on_message(message):
 """
 SLASH COMMANDS
 """
-
 
 # /weird will have dinkybot post the "this is really weird" GIF
 @dinkybot.tree.command(name="weird", description="Posts the 'this is weird' GIF")
@@ -62,4 +65,4 @@ async def dy(interaction: discord.Interaction):
     await utils.post_gif("dy-optimized", interaction)
 
 
-dinkybot.run(os.getenv("DISCORD_TOKEN"))
+dinkybot.run(os.getenv("BOT_TOKEN"))
