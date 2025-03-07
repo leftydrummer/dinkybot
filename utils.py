efa5d9e4e6222765f92c55891817eb3d171091d2
Neill Wolf <neillawolf@gmail.com>
2025-03-07 17:16:55 -0600

    improve weird trigger, add LOTR easter egg

--- utils.py
+++ utils.py
@@ -1,10 +1,17 @@
 import discord
 import os
 
+import nltk.sentiment
+import nltk.sentiment.vader
+import constants
+import nltk
+
+nltk.download("vader_lexicon", quiet=True)
+sentiment_analyzer = nltk.sentiment.vader.SentimentIntensityAnalyzer()
 
 # locate a GIF by name in the assets folder and post it given a name and the discord interaction context
 async def post_gif(gif_name: str, context):
-    gif_path = os.path.join('assets', f"{gif_name}.gif")
+    gif_path = os.path.join("assets", f"{gif_name}.gif")
     if os.path.exists(gif_path):
         file = discord.File(gif_path)
         if isinstance(context, discord.Interaction):
@@ -14,3 +21,11 @@ async def post_gif(gif_name: str, context):
     else:
         print(f"File {gif_path} does not exist.")
         return
+
+
+# returns bool indicating if the message contains anti-lotr sentiment
+async def contains_anti_lotr_sentiment(message: str):
+    return (
+        any(keyword in message.lower() for keyword in constants.lotr_keywords)
+        and sentiment_analyzer.polarity_scores(message)["compound"] < 0
+    )
